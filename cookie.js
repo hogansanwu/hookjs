3b4479eb5e3c206397f86610c8eb2831100c091c
hogansanwu <9728098@qq.com>
2025-03-18 22:03:59 +0800

    Add files via upload
    hook cookie

---
+++ cookie.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        hook_cookie
+// @version     2025-03-18
+// @description try to take over the world!
+// @author      Hogan Pei
+// @match       https://*/*
+// @grant       none
+// @homepageURL https://gitee.com/hoganpei/jshook
+// @run-at      document-start
+// ==/UserScript==
+
+var ckstr = 'v=';
+
+(function () {
+  'use strict';
+  var cookieTemp = '';
+  Object.defineProperty(document, 'cookie', {
+    set: function (val) {
+      if (val.indexOf(ckstr) != -1) {
+        debugger;
+      }
+      console.log('cookie set->', val);
+      cookieTemp = val;
+      return val;
+    },
+    get: function () {
+      return cookieTemp;
+    },
+  });
+})();
+
+(function () {
+    'use strict';
+    var org = document.cookie.__lookupSetter__('cookie');
+    document.__defineSetter__('cookie', function (cookie) {
+        if (cookie.indexOf(ckstr) != -1) {
+            debugger;
+        }
+        org = cookie;
+    });
+    document.__defineGetter__('cookie', function () {
+        return org;
+    });
+})();
